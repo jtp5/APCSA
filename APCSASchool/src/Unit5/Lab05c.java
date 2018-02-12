54e9879c0c347fa58ee821f540969837ca0cb951
pearlmanj9322 <pearlmanj9322@CA-SU-F106-40.sduhsd.lan>
2018-02-12 14:58:23 -0800

    Added unit 5 stuff and did the lab

---
+++ APCSASchool/src/Unit5/Lab05c.java
@@ -0,0 +1,22 @@
+package Unit5;
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+import java.util.Scanner;
+
+public class Lab05c
+{
+	public static void main ( String[] args )
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a word");
+		String w = input.nextLine();
+		
+		StringOddOrEven test = new StringOddOrEven(w);
+		System.out.println(test);
+	}
+}
