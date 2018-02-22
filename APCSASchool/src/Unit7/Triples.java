62247e9d3f21863adbcbf41f81a5e4144e4befc0
pearlmanj9322 <pearlmanj9322@CA-SU-F106-40.sduhsd.lan>
2018-02-22 14:59:31 -0800

    triples

--- APCSASchool/src/Unit7/Triples.java
+++ APCSASchool/src/Unit7/Triples.java
@@ -32,7 +32,7 @@ public class Triples
 	private int greatestCommonFactor(int a, int b, int c)
 	{
 		int max = 1;
-		for (int i = 0; i < Math.sqrt(a); i++) {
+		for (int i = 1; i <= 9000; i++) {
 			if(a % i == 0 && b % i == 0 && c % i == 0){
 				max = i;
 			}
@@ -44,14 +44,39 @@ public class Triples
 
 	public String toString()
 	{
-		int num1 = 3;
-		int num2 = 4;
-		int num3 = 5;
+		int num1 = 0;
+		int num2 = 0;
+		int num3 = 0;
 		String output="";
 		
-		for (int i = num1; i <= number; i++) {
-			while(!(greatestCommonFactor(num1, num2, 1) == 1)){
+		for (int i = 1; i <= number; i++) {
+			
+			for (int j = 1; j <= number; j++) {
 				
+				for (int k = 1; k <= number; k++) {
+					
+					if((Math.pow(i, 2) + Math.pow(j, 2) == Math.pow(k, 2))){
+						if(i % 2 == 0){
+							if(j % 2 == 1){
+								if(greatestCommonFactor(i, j, k) == 1){
+									if (j > i){
+										output+= i + " " + j + " " + k + "\n";
+									}
+									
+								}
+							}
+						}
+						else{
+							if(j % 2 == 0){
+								if(greatestCommonFactor(i, j, k) == 1){
+									if(j > i){
+									output+= i + " " + j + " " + k + "\n";
+									}
+								}
+							}
+						}
+					}
+				}
 			}
 		}
 
