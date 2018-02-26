c74d6c653793ff68ba29e8b3f20652a782a646f9
pearlmanj9322 <pearlmanj9322@CA-SU-F106-40.sduhsd.lan>
2018-02-26 14:52:29 -0800

    added unit9 stuff

---
+++ APCSASchool/src/Unit9/CardTester.java
@@ -0,0 +1,16 @@
+package Unit9;
+/**
+ * This is a class that tests the Card class.
+ */
+public class CardTester {
+
+	/**
+	 * The main method in this class checks the Card operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card aceOfSpades = new Card("Ace", "Spades", 1);
+		System.out.println(aceOfSpades);
+	}
+}
