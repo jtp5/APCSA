fff2a64bf3c96b621b3a030cb7ce90d53e1d0c98
jtp5 <jones.pearlman@gmail.com>
2018-03-19 14:59:15 -0700

    kept working

--- APCSASchool/src/Unit13/InsertionSort.java
+++ APCSASchool/src/Unit13/InsertionSort.java
@@ -1,52 +1,49 @@
 package Unit13;
 //© A+ Computer Science  -  www.apluscompsci.com
+
 //Name -
 //Date -
 //Class - 
 //Lab  - 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import static java.lang.System.*;
 
-class InsertionSort
-{
-	private ArrayList<String> list;
+class InsertionSort {
+	private ArrayList<String> list = new ArrayList<String>();
 
-	public InsertionSort()
-	{
-
-
+	public InsertionSort() {
 
 	}
 
-	//modfiers
-	public void add( String  word)
-	{
+	// modfiers
+	public void add(String word) {
 		int loc = 0;
+		
+		if (Math.abs(Collections.binarySearch(list, word)) < list.size()) {
+			list.add(Math.abs(Collections.binarySearch(list, word)), word);
+		}
+		else if(Collections.binarySearch(list, word) < 0){
+			list.add(word);
+		}
+	}
 
+	public void remove(String word) {
 
-
-
-
-
-
+		if (Collections.binarySearch(list, word) >= 0) {
+			System.out.println(list.get(Collections.binarySearch(list, word)));
+			list.remove(Collections.binarySearch(list, word));
+		}
 
 	}
 
-
-	public void remove(String word)
-	{
-
-
-
-
-
-
-	}
-
-	public String toString()
-	{
-		return "";
+	public String toString() {
+		String output = "";
+		for(String s : list){
+			output += (s + "\n");
+		}
+		return output;
 	}
 }
