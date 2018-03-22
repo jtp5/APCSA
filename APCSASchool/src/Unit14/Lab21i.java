d6877187d82d26639f60981db67e12bee8f2feff
jtp5 <jones.pearlman@gmail.com>
2018-03-22 14:57:51 -0700

    worked on unit 14

--- APCSASchool/src/Unit14/Lab21i.java
+++ APCSASchool/src/Unit14/Lab21i.java
@@ -7,6 +7,7 @@ package Unit14;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 import static java.lang.System.*;
 
@@ -14,5 +15,35 @@ public class Lab21i
 {
 	public static void main( String args[] ) throws IOException
 	{
+		Scanner file = new Scanner(new File(System.getProperty("user.dir")+"\\src\\Unit14\\lab21i.dat"));
+		Maze test = new Maze(Integer.parseInt(file.nextLine()), file.nextLine());
+		for (int i = 0; i < 5; i++) {
+			System.out.println(Arrays.toString(test.getMaze()[i]));
+		}
+		System.out.println(test);
+		
+		System.out.println("");
+		
+		Maze test2 = new Maze(Integer.parseInt(file.nextLine()), file.nextLine());
+		for (int i = 0; i < 5; i++) {
+			System.out.println(Arrays.toString(test2.getMaze()[i]));
+		}
+		System.out.println(test2);
+		
+		System.out.println("");
+		
+		Maze test3 = new Maze(Integer.parseInt(file.nextLine()), file.nextLine());
+		for (int i = 0; i < 5; i++) {
+			System.out.println(Arrays.toString(test3.getMaze()[i]));
+		}
+		System.out.println(test3);
+		
+		System.out.println("");
+		
+		Maze test4 = new Maze(Integer.parseInt(file.nextLine()), file.nextLine());
+		for (int i = 0; i < 5; i++) {
+			System.out.println(Arrays.toString(test4.getMaze()[i]));
+		}
+		System.out.println(test4);
 	}
 }
--- APCSASchool/src/Unit14/Maze.java
+++ APCSASchool/src/Unit14/Maze.java
@@ -16,27 +16,58 @@ public class Maze
 
 	public Maze()
 	{
-
+		maze = new int[][]{{1, 0, 0, 0, 0}, {1, 1, 1, 1, 1}, {0, 0, 0, 0, 0}};
 
 	}
 
 	public Maze(int size, String line)
 	{
-
+		maze = new int[size][size];
+		int track = 0;
+		
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				maze[i][j] = (int)(line.charAt(track)) - 48;
+				track = track + 2;
+			}
+		}
 
 	}
 
 	public boolean hasExitPath(int r, int c)
 	{
-
+		int save = 0;
+		if((r < maze.length && r > -1) && (c < maze[r].length && c > -1) && (maze[r][c] == 1)){
+			save = maze[r][c];
+			maze[r][c] = 2;
+			hasExitPath(r - 1, c);
+			hasExitPath(r + 1, c);
+			hasExitPath(r, c - 1);
+			hasExitPath(r, c + 1);
+		}
+		if(c == maze.length - 1){
+			return true;
+		}
+		if((r < maze.length && r > -1) && (c < maze[r].length && c > -1)){
+			maze[r][c] = save;
+		}
 		return false;
 	}
 
+	public int[][] getMaze(){
+		return maze;
+	}
+	
 	public String toString()
 	{
 		String output="";
 
-
+		if(hasExitPath(0, 0)){
+			output += "exit found";
+		}
+		else{
+			output += "exit not found";
+		}
 
 
 
